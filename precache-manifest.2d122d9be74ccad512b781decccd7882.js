ca7cc7f5f70ee26569c5afa6527aed2bc61d21ae
venkateshkocherlakota <kocherlakotavenkatesh666@gmail.com>
2019-07-09 18:48:02 +0530

    Test React JS App

---
+++ precache-manifest.2d122d9be74ccad512b781decccd7882.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b685716bf8fec553255706ce34fcbcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ad2f8da4a1dcd16093b",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "3c30e55bd904f2a3a746",
+    "url": "/static/css/main.b2e33d63.chunk.css"
+  },
+  {
+    "revision": "1ad2f8da4a1dcd16093b",
+    "url": "/static/js/2.994d2e6e.chunk.js"
+  },
+  {
+    "revision": "3c30e55bd904f2a3a746",
+    "url": "/static/js/main.65c70aec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c6c62d523b3f69a6147306af6b5d6e7c",
+    "url": "/static/media/1.c6c62d52.png"
+  },
+  {
+    "revision": "5f4651d96fdd12cab42a59e329f94900",
+    "url": "/static/media/2.5f4651d9.png"
+  },
+  {
+    "revision": "e87e808aafd9f7dfa8c7c2348cd39f1e",
+    "url": "/static/media/3.e87e808a.png"
+  }
+]);
