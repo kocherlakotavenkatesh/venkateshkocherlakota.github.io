479a0677b0d562cc0b064464b130a696aa81c5dc
VenkateshKocherlakota <43877016+venkateshkocherlakota@users.noreply.github.com>
2020-04-22 19:13:20 +0530

    Major design updates
    This is more cooler B-)

---
+++ precache-manifest.9b50671314fdee7701bf8a0e19883d0b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5954c0ee1653bc894fc6c371706b880a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5e1831a2c53bab541c5",
+    "url": "/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "1a0cc8693962796bac68",
+    "url": "/static/css/main.63048c34.chunk.css"
+  },
+  {
+    "revision": "e5e1831a2c53bab541c5",
+    "url": "/static/js/2.09b6fae9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09b6fae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a0cc8693962796bac68",
+    "url": "/static/js/main.457d22eb.chunk.js"
+  },
+  {
+    "revision": "f3820227af9c652b5fb2",
+    "url": "/static/js/runtime-main.fa239ec1.js"
+  },
+  {
+    "revision": "9aef94cebb4153e69712951381ceeced",
+    "url": "/static/media/down-arrow.9aef94ce.svg"
+  },
+  {
+    "revision": "7008915c03ccbd3804dcfa729c57555d",
+    "url": "/static/media/profile.7008915c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66f3c893aff5230879988cec175a9916.js"
+  "/precache-manifest.9b50671314fdee7701bf8a0e19883d0b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
